8edc264d8fbb9021c10fc8831be6d6151fa1ae74
Tom Oddy <tom_oddy@live.co.uk>
2026-04-18 21:50:45 +0100

    Added key generator

---
+++ KeyGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Generate key
+        Console.WriteLine("Generating key...");
+        Guid guid = Guid.NewGuid();
+
+        // Hash key
+        Console.WriteLine("Hashing key...");
+        byte[] keyBytes = SHA256.HashData(guid.ToByteArray());
+        string key = Convert.ToBase64String(keyBytes);
+
+        // Write hashed key to file
+        Console.WriteLine("Saving key to file...");
+        File.AppendAllLines(args[0], [key]);
+
+        // Output key to console
+        Console.WriteLine("Key generated and saved to file.");
+        Console.WriteLine("Key: " + guid.ToString());
+    }
+}
